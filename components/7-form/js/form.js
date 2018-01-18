d36f7b2cf925920469586b575681159274cef56b
brunavieirat <41126857858@jpmorgan.sp>
2018-01-18 21:49:37 -0200

    Add Items OK

--- components/7-form/js/form.js
+++ components/7-form/js/form.js
@@ -1,7 +1,7 @@
 var items = [];
 
 
-function teste() {
+function adicionarItens() {
 
     event.preventDefault();
 
@@ -17,15 +17,36 @@ function teste() {
     var div = document.createElement('div');
     div.id = "item";
 
+    
+
     var template = "";
     template += '<div class = "form__item"> ITEM </div>';
-    template += '<div class="form__item-num"> ' + items.length + ' </div>';
+    template += '<div class="form__item-num">' + items.length + '</div>';
     template += ' <div> <input type="text" class="form__item-add"></div>';
     template += '<div class="form__button-itemremove">-</div>';
+   
 
     div.innerHTML = template;
 
-    document.getElementById("itens").appendChild(div);
+    document.getElementById("lista-itens").appendChild(div);
+}
+
+
+function validarForm(){
+
+    var template = "";
+
+     template += '<button type = "submit" class="form__button"> Cadastrar c/ DOM </button>';
+     template += '<button type = "submit" class="form__button"> Cadastrar s/ DOM </button>';
+     template += '<button type = "submit" class="form__button"> Limpar </button>';
+
+     document.getElementById("form__buttons").innerHTML=template;
+
+}
+
+function removerItems(){
+
+
 }
 
 
